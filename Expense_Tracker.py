5a0cbc98774b63ef4cedbe946e42d74bd21e802f
nickd10 <nickdossin@gmail.com>
2023-04-18 15:22:39 -0400

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -27,4 +27,38 @@ def pay_credit():
     pass
 
 def main():
-    pass
+    """Main function of the program"""
+    Continue = True
+    while Continue:
+        print("Welcome to the Expense Tracker!")
+        print("Please select an option from the menu below:")
+        print("1. Add an expense")
+        print("2. Delete an expense")
+        print("3. Calculate yearly tax")
+        print("4. Calculate average expense by category")
+        print("5. Calculate total expenses")
+        print("6. Calculate expenses by category")
+        print("7. Calculate expenses per month")
+        print("8. Pay credit")
+        print("9. Exit")
+        option = input("Please enter your selection: ")
+        if option == "1":
+            add_expense()
+        elif option == "2":
+            delete_expense()
+        elif option == "3":
+            tax_calculation()
+        elif option == "4":
+            average_expense()
+        elif option == "5":
+            total_expenses()
+        elif option == "6":
+            expense_by_category()
+        elif option == "7":
+            expense_per_month()
+        elif option == "8":
+            pay_credit()
+        elif option == "9":
+            Continue = False
+        else:
+            print("Invalid option, please try again.")
