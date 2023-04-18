89f204794971265b3090b7fd8c2530119e9cb89d
nickd10 <nickdossin@gmail.com>
2023-04-18 15:08:34 -0400

    Create Expense_Tracker.py

---
+++ Expense_Tracker.py
@@ -0,0 +1,26 @@
+def add_expense():
+    pass
+
+def delete_expense():
+    pass
+
+def tax_calculation():
+    pass
+
+def average_expense():
+    pass
+
+def total_expenses():
+    pass
+
+def expense_by_category():
+    pass
+
+def expense_per_month():
+    pass
+
+def pay_credit():
+    pass
+
+def main():
+    pass
