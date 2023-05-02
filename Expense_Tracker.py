4b040f8578ef05408d48c85feace091fcb617dc5
nabihathar <129790230+nabihathar@users.noreply.github.com>
2023-05-01 21:02:54 -0400

    Update Expense_Tracker.py

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -16,10 +16,12 @@ def add_expense(expense_name, expense_amount, expense_dict):
     total_expense = sum(expenses_dict.values())
     return total_expense
 
-def delete_expense():
+def delete_expense(expense, name):
     """Will delete an expense name and amount (string, float) from a dictionary of expenses,
     append it, and will calculate and return the new total expense amount"""
-    pass
+    amount = expense.pop(name)
+    total = sum(expense.values())
+    return amount, total
 
 def tax_calculation(income):
     """Calculates the yearly tax expense of the user based on their
